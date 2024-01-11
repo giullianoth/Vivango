ed6ed21c47acd1a10ec2a2775f23f5454f443d31
Giulliano Guimarães <llthguimaraes@gmail.com>
2024-01-11 16:39:32 -0300

    responsividade - dobra aventure-se

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,16 @@
-const navbar_toggler = document.querySelector(".j_navbar_toggler")
+const navbar_toggler = document.querySelector(".j_navbar_toggler"),
+    adventure_tabs = document.querySelector(".j_adventure_tabs")
 
-navbar_toggler.addEventListener("click", ({ target }) => {
+const adventure_tabs_links = adventure_tabs.querySelectorAll(".nav-link")
+
+navbar_toggler && navbar_toggler.addEventListener("click", ({ target }) => {
     target.classList.toggle("active")
+})
+
+adventure_tabs_links && adventure_tabs_links.forEach((link, i, arr) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault()
+        arr.forEach(item => link !== item && item.classList.remove("active"))
+        link.classList.add("active")
+    })
 })
